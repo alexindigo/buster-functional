03d243275a3108e9af8cf59bc3403ca72e3dc046
Alex Indigo <iam@alexindigo.com>
2015-02-19 21:23:05 -0800

    Added loader tests

--- test/test-mixin_loader.js
+++ test/test-mixin_loader.js
@@ -26,5 +26,51 @@ buster.testCase('Loader',
   'Attaches onCreate hanlder': function()
   {
     assert.calledOnce(this.stubBuster.testRunner.onCreate);
+  },
+
+  'Listens to the runner events:':
+  {
+    setUp: function()
+    {
+      // stub test runner object
+      this.runner = {on: this.spy()};
+
+      // stub global busterFunctionalMixin
+      global.busterFunctionalMixin = {me: Math.random()};
+
+      // provide runner object to the onCreate callback
+      this.stubBuster.testRunner.onCreate.getCall(0).args[0](this.runner);
+    },
+
+    'Attaches test runner event listners': function()
+    {
+      // it assigns handler for several events
+      assert.calledWith(this.runner.on, 'suite:start');
+      assert.calledWith(this.runner.on, 'suite:end');
+      assert.calledWith(this.runner.on, 'context:end');
+      // attaches the mixin to the runner setup
+      assert.calledWith(this.runner.on, 'test:setUp', global.busterFunctionalMixin);
+    },
+
+    'Resizes frames on suite:start': function()
+    {
+      var targetFrameset = {rows: '_UNMODIFIED_'}
+        , stubGetElementsByTagName = this.stub()
+        ;
+
+      // prepare stub
+      stubGetElementsByTagName.withArgs('frameset').returns([targetFrameset]);
+
+      // traverse dom
+      global.window = {top: {document: {getElementsByTagName: stubGetElementsByTagName}}};
+
+      // first one is 'suite:start'
+      this.runner.on.getCall(0).args[1]();
+
+      // check frameset selection
+      assert.calledWith(stubGetElementsByTagName, 'frameset');
+      // top iframe set to 0
+      assert.equals(targetFrameset.rows, '0,*');
+    }
   }
 });
